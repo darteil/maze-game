5d25f5e9f5a1c43c4441034ed856fc6659a37b4b
Romanov Yuri <darteil.base@gmail.com>
2020-04-06 14:07:23 +0700

    second light source added

--- src/scene.ts
+++ src/scene.ts
@@ -2,7 +2,8 @@ import * as BABYLON from 'babylonjs';
 
 export default class SceneInstance {
   public scene: BABYLON.Scene;
-  public directionalLight: BABYLON.DirectionalLight;
+  public directionalLight_01: BABYLON.DirectionalLight;
+  public directionalLight_02: BABYLON.DirectionalLight;
   public hemisphericLight: BABYLON.HemisphericLight;
   // public shadowGenerator: BABYLON.ShadowGenerator;
 
@@ -12,12 +13,21 @@ export default class SceneInstance {
     this.hemisphericLight = new BABYLON.HemisphericLight('HemisphericLight', new BABYLON.Vector3(0, 1, 0), this.scene);
     this.hemisphericLight.intensity = 0.6;
     this.hemisphericLight.specular = BABYLON.Color3.Black();
-    this.directionalLight = new BABYLON.DirectionalLight(
+
+    this.directionalLight_01 = new BABYLON.DirectionalLight(
       'DirectionalLight_01',
       new BABYLON.Vector3(-200, -200, -200),
       this.scene,
     );
-    this.directionalLight.intensity = 0.7;
+    this.directionalLight_01.intensity = 0.7;
+
+    this.directionalLight_02 = new BABYLON.DirectionalLight(
+      'DirectionalLight_02',
+      new BABYLON.Vector3(200, 200, 200),
+      this.scene,
+    );
+    this.directionalLight_02.intensity = 0.7;
+
     // this.shadowGenerator = new BABYLON.ShadowGenerator(1024, this.directionalLight);
     // this.shadowGenerator.useBlurCloseExponentialShadowMap = true;
     // this.shadowGenerator.forceBackFacesOnly = true;
