a78839add65ffbdc9faa5cc5ea38afe7ab5841f1
Romanov Yuri <darteil.base@gmail.com>
2021-04-08 10:41:00 +0700

    Update webpack plugins

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,20 +1,20 @@
-const path = require("path");
-const merge = require("webpack-merge");
-const base = require("./webpack.base.js");
+const path = require('path');
+const { merge } = require('webpack-merge');
+const base = require('./webpack.base.js');
 
-const publicPath = path.resolve(__dirname, "../public");
+const publicPath = path.resolve(__dirname, '../public');
 
 module.exports = (env, argv) =>
   merge(base(env, argv), {
-    devtool: "source-map",
+    devtool: 'source-map',
     output: {
-      chunkFilename: "[name].chunk.js"
+      chunkFilename: '[name].chunk.js',
     },
     devServer: {
       contentBase: publicPath,
-      stats: "errors-only",
+      stats: 'errors-only',
       overlay: true,
       port: 4000,
-      historyApiFallback: true
-    }
+      historyApiFallback: true,
+    },
   });
